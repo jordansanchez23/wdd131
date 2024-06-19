52ff86fe47c3e44aa03811deb249b3e713c2b79d
jordansanchez23 <joivisvis@hotmail.com>
2024-06-19 14:50:08 -0700

    bom-part2

---
+++ week02/scripts/bom.js
@@ -0,0 +1,37 @@
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+button.addEventListener('click', function() {
+    
+    if (input.value.trim() !== '') {
+        
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+
+        
+        li.append(deleteButton);
+
+        
+        list.append(li);
+
+        
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus(); 
+        });
+
+        
+        input.value = '';
+        input.focus();
+    } else {
+        
+        input.focus();
+    }
+});
