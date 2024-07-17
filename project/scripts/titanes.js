3932719f79c859b3924b87c0e5e77e840c2195dc
jordansanchez23 <joivisvis@hotmail.com>
2024-07-17 16:33:02 -0700

    index done

--- project/scripts/titanes.js
+++ project/scripts/titanes.js
@@ -7,3 +7,15 @@ const lastModified = document.lastModified;
 
 const lastModifiedElement = document.getElementById("lastModified");
 lastModifiedElement.textContent = `Last modified ${lastModified}`;
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuButton = document.getElementById("menu");
+    const nav = document.querySelector(".navigation");
+    const header = document.querySelector("header");
+
+    menuButton.addEventListener("click", function() {
+        menuButton.classList.toggle("open");
+        nav.classList.toggle("open");
+        header.classList.toggle("menu-open"); 
+    });
+});
